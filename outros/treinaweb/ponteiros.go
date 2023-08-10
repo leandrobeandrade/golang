0a3f78b8a192b07e7aca7c981c78aeb215cbde60
leandrobeandrade <leandrobe.andrade@gmail.com>
2023-08-09 21:27:31 -0300

    Ponteiros

---
+++ outros/treinaweb/ponteiros.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func mudaValor(a *int) {
+	*a = 20
+}
+
+func main() {
+	var a int = 10
+
+	fmt.Printf("Valor de a: %d\n", a)
+	mudaValor(&a)
+	fmt.Printf("Valor de a: %d\n", a)
+}
