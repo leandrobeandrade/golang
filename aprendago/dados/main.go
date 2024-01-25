670b161a4056fd7d9d5108f9c3d0ea9d97fe8190
leandrobeandrade <leandrobe.andrade@gmail.com>
2024-01-25 18:18:29 -0300

    Aprenda Go

--- aprendago/dados/main.go
+++ aprendago/dados/main.go
@@ -4,8 +4,8 @@ import "fmt"
 
 var arr1 [5]int                     // array de inteiros de tamanho 5
 var arr2 = [6]int{1, 2, 3, 4, 5, 6} // array inicializado
-// var sli1 []int                      // slice de tamanho indeterminado
-var sli2 = []int{1, 2, 3} // array inicializado
+var sli1 []int                      // slice de tamanho indeterminado
+var sli2 = []int{1, 2, 3}           // slice inicializado
 
 func main() {
 	fmt.Println("=== Array ===")
@@ -17,8 +17,34 @@ func main() {
 	fmt.Println(len(arr1))
 
 	fmt.Println("=== Slice ===")
-	sli1 := append(sli2, 2)
-	fmt.Println(sli1, sli2)
-	fmt.Printf("%T %T\n", sli1, sli2)
-	fmt.Println(len(sli1))
+	sli1 = append(sli1, 1, 2, 3)
+	fmt.Println("Slice1", sli1)
+
+	sli3 := append(sli2, 2)
+	fmt.Println("Slice3 - Slice2", sli3, "\t", sli2)
+	fmt.Printf("%T %T\n", sli3, sli2)
+	fmt.Println(len(sli3))
+
+	fmt.Println("=== Slice com range ===")
+	sli4 := []string{"banana", "maçã", "jaca"}
+	for idx, val := range sli4 {
+		fmt.Println("No índice:", idx, "temos o valor:", val)
+	}
+
+	sli5 := []int{1, 2, 3}
+	var total int = 0
+	for _, val := range sli5 {
+		fmt.Println(val)
+		total = total * val
+	}
+	fmt.Println("\nValor total:", total)
+	fmt.Println()
+
+	sabores := []string{"pepperoni", "mozzarela", "abacaxi", "quatroqueijos", "marguerita"}
+	fmt.Println(sabores[0])
+	fmt.Println(sabores[0:2])
+	fmt.Println(sabores[:2])
+	fmt.Println(sabores[2:])
+	// fmt.Println(sabores[2:len(sabores)])
+	fmt.Println(sabores[:])
 }
