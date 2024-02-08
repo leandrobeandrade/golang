2ab8326674f9aff81755945872189bb625db513c
leandrobeandrade <leandrobe.andrade@gmail.com>
2024-02-08 17:20:21 -0300

    Aprenda Go

---
+++ aprendago/concoparal/mai.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	// Roda sequencialmente as 2 funções
+	/* um()
+	dois() */
+
+	// fmt.Println()
+
+	// Roda apenas a função dois pq chega no final da main e para a execução
+	// Função um fica aguardando e não roda
+	/* go um()
+	dois() */
+
+	// fmt.Println()
+
+	// Roda as 2 funções sequencialmente
+	/* wg.Add(1)
+	go um()
+	dois()
+	wg.Wait() */
+
+	// Roda as 2 funções concorrentemente
+	wg.Add(2)
+	go um()
+	go dois()
+	wg.Wait()
+}
+
+func um() {
+	for i := 1; i <= 5; i++ {
+		fmt.Println("Um:", i)
+		time.Sleep(1000)
+	}
+	wg.Done()
+}
+
+func dois() {
+	for i := 1; i <= 5; i++ {
+		fmt.Println("Dois:", i)
+		time.Sleep(1000)
+	}
+	wg.Done()
+}
