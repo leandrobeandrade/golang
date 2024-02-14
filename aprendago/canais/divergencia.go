1340e2a4cbe009d4f6849c312c9b3c407ea230ac
leandrobeandrade <leandrobe.andrade@gmail.com>
2024-02-14 00:11:47 -0300

    Aprenda Go

--- aprendago/canais/divergencia.go
+++ aprendago/canais/divergencia.go
@@ -18,6 +18,7 @@ func divergencia() {
 		fmt.Println(v)
 	}
 
+	outraDivergencia()
 }
 
 func manda(n int, canal chan int) {
@@ -43,5 +44,46 @@ func outra(canal1, canal2 chan int) {
 
 func trabalho_(n int) int {
 	time.Sleep(time.Millisecond * time.Duration(rand.Intn(1e3)))
+	return n
+}
+
+// Segundo exemplo
+func outraDivergencia() {
+	canal1 := make(chan int)
+	canal2 := make(chan int)
+	funções := 5
+
+	go manda_(15, canal1)
+	go outra_(funções, canal1, canal2)
+
+	for v := range canal2 {
+		fmt.Println(v)
+	}
+}
+
+func manda_(n int, canal chan int) {
+	for i := 0; i < n; i++ {
+		canal <- i
+	}
+	close(canal)
+}
+
+func outra_(funções int, canal1, canal2 chan int) {
+	var wg sync.WaitGroup
+	for i := 0; i < funções; i++ {
+		wg.Add(1)
+		go func() {
+			for v := range canal1 {
+				canal2 <- trabalho__(v)
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	close(canal2)
+}
+
+func trabalho__(n int) int {
+	time.Sleep(time.Millisecond * 1000) //time.Duration(rand.Intn(1e3)))
 	return n
 }
