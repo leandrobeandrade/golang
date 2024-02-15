544962e2c4988bc0f5fc01fb6fdac97bf7a5a30e
leandrobeandrade <leandrobe.andrade@gmail.com>
2024-02-15 00:40:56 -0300

    Aprenda Go

--- aprendago/canais/context.go
+++ aprendago/canais/context.go
@@ -13,10 +13,40 @@ func context_() {
 	fmt.Printf("context type:\t%T\n", ctx)
 	fmt.Println("----------")
 
-	ctx, _ = context.WithCancel(ctx)
+	withCancel()
+
+	lang1 := lang{language: "Go"}
+	lang2 := lang{
+		language: "Python",
+	}
+
+	ctx2 := context.WithValue(context.Background(), lang1, "Go")
+	withValue(ctx2, "language")
+
+	ctx2 = context.WithValue(ctx2, lang2, "Python")
+	withValue(ctx2, "language")
+	withValue(ctx2, "other")
+}
+
+func withCancel() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	defer cancel()
 
 	fmt.Println("context:\t", ctx)
 	fmt.Println("context err:\t", ctx.Err())
 	fmt.Printf("context type:\t%T\n", ctx)
 	fmt.Println("----------")
+}
+
+func withValue(ctx context.Context, s string) {
+	if v := ctx.Value(s); v != nil {
+		fmt.Println("found value:", v)
+		return
+	}
+	fmt.Println("key not found:", s)
+}
+
+type lang struct {
+	language string
 }
